2492a9399ff6fbd579d52dbb8c9b0d1f211534d2
Shnaider Danila <shnd3r@yandex.ru>
2023-08-18 14:00:17 +0700

    fix: fixed getUser controller function in users.js

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ module.exports.getAllUsers = (req, res) => {
 };
 
 module.exports.getUser = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.userId)
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
