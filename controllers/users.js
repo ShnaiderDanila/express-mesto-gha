6b75abaacb71f28bec6b88b19bdd69a076dcd112
Shnaider Danila <shnd3r@yandex.ru>
2023-08-18 16:00:44 +0700

    feat: Add new functions controllers updateUserProfile and updateUserAvatar

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,44 @@
 const User = require('../models/user');
 
-module.exports.getAllUsers = (req, res) => {
+const updateOptions = {
+  new: true,
+  runValidators: true,
+  upsert: true,
+};
+
+const getAllUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports.getUser = (req, res) => {
+const getUser = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports.createUser = (req, res) => {
+const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const updateUserProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, about }, updateOptions)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, updateOptions)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports = {
+  getAllUsers, getUser, createUser, updateUserProfile, updateUserAvatar,
 };
