e028d0292476edd70d9f745fb8b9b65588eaf362
Shnaider Danila <shnd3r@yandex.ru>
2023-08-18 15:20:56 +0700

    feat: Add controller cards.js file

---
+++ controllers/cards.js
@@ -0,0 +1,24 @@
+const Card = require('../models/card');
+
+const getCards = (req, res) => {
+  Card.find({})
+    .populate('owner')
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const createCard = (req, res) => {
+  const { name, link } = req.body;
+  const ownerId = req.user._id;
+  Card.create({ name, link, owner: ownerId })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const deleteCard = (req, res) => {
+  Card.findByIdAndRemove(req.params.cardId)
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports = { getCards, createCard, deleteCard };
