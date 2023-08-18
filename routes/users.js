3d1fc5e717c3356da28f94fbb918954de34261cc
Shnaider Danila <shnd3r@yandex.ru>
2023-08-18 19:19:27 +0700

    feat: Renamed controller function from getAllUsers to getUsers in ./routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const {
-  getAllUsers, getUser, createUser, updateUserProfile, updateUserAvatar,
+  getUsers, getUser, createUser, updateUserProfile, updateUserAvatar,
 } = require('../controllers/users');
 
-router.get('/', getAllUsers);
+router.get('/', getUsers);
 router.get('/:userId', getUser);
 router.post('/', createUser);
 router.patch('/me', updateUserProfile);
