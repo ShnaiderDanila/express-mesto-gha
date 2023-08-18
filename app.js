8847a572fca4d9ab3e2631a2489d8e79645e1ef6
Shnaider Danila <shnd3r@yandex.ru>
2023-08-18 14:02:12 +0700

    feat: Add routes directory with users.js router and include it in app.js

--- app.js
+++ app.js
@@ -3,17 +3,14 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
-
-const { PORT = 3000 } = process.env;
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
