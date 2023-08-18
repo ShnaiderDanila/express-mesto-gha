c4b5788f9be9133a9d44bacd4ba0aceea9511674
Shnaider Danila <shnd3r@yandex.ru>
2023-08-18 19:21:10 +0700

    refactor: Remove console.log in app.js

--- app.js
+++ app.js
@@ -22,6 +22,4 @@ app.use('/users', require('./routes/users'));
 
 app.use('/cards', require('./routes/cards'));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT, () => {});
