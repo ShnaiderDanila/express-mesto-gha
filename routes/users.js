2daad4634ce4ba93aee3e00cdbffaa8c2707967f
Shnaider Danila <shnd3r@yandex.ru>
2023-08-18 16:03:35 +0700

    feat: Add new patch routers to ./routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const { getAllUsers, getUser, createUser } = require('../controllers/users');
+const {
+  getAllUsers, getUser, createUser, updateUserProfile, updateUserAvatar,
+} = require('../controllers/users');
 
 router.get('/', getAllUsers);
 router.get('/:userId', getUser);
 router.post('/', createUser);
+router.patch('/me', updateUserProfile);
+router.patch('/avatar', updateUserAvatar);
 
 module.exports = router;
