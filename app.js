2657dc7ae7be7e66c2b5a1f76c598363e77c4799
Shnaider Danila <shnd3r@yandex.ru>
2023-08-22 18:11:07 +0700

    fix: Remove the body-parser package from the project and replace it with express.json in app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   req.user = {
