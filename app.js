db1fd19e2f8cf7b95869cea2b43e13e29ad537aa
Shnaider Danila <shnd3r@yandex.ru>
2023-09-03 14:35:42 +0700

    fix: Remove hardcode in app.js

--- app.js
+++ app.js
@@ -17,13 +17,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '64e5bcf49527a85415e0622f',
-  };
-  next();
-});
-
 app.post('/signin', login);
 app.post('/signup', createUser);
 
