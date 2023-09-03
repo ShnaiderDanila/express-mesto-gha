4b980f48908421988150c8e5f193810936a9f41c
Shnaider Danila <shnd3r@yandex.ru>
2023-09-03 13:37:51 +0700

    feat: Add npm cookie-parser module and include it in app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const routes = require('./routes/index');
 const { createUser, login } = require('./controllers/users');
 
 const app = express();
+
+app.use(cookieParser());
+
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
