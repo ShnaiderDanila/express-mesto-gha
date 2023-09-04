1587ca90360d224a160aca94cf9221a7fd64e717
Shnaider Danila <shnd3r@yandex.ru>
2023-09-04 15:08:05 +0700

    feat: Replace error handling in catch blocks of all controllers with centralized error handling in ./controllers/cards.js and /.controllers/users.js and remove old code

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,86 +1,60 @@
-const mongoose = require('mongoose');
 const Card = require('../models/card');
+const NotFoundError = require('../errors/NotFoundError');
+const ForbiddenError = require('../errors/ForbiddenError');
 
-const DEFAULT_ERROR = 500;
-const NOT_FOUND_ERROR = 404;
-const FORBIDDEN_ERROR = 403;
-const BAD_REQUEST_ERROR = 400;
 const CREATED_STATUS = 201;
 const OK_STATUS = 200;
 
-const getCards = (req, res) => {
+const getCards = (req, res, next) => {
   Card.find({})
     .populate(['owner', 'likes'])
     .then((cards) => {
       res.send(cards);
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
-    });
+    .catch(next);
 };
 
-const createCard = (req, res) => {
+const createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.status(CREATED_STATUS).send(card))
-    .catch((err) => {
-      console.error(err);
-      if (err instanceof mongoose.Error.ValidationError) {
-        return res.status(BAD_REQUEST_ERROR).send({ message: 'Переданы некорректные данные при создании карточки.' });
-      }
-      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
-    });
+    .catch(next);
 };
 
-const deleteCard = (req, res) => {
+const deleteCard = (req, res, next) => {
   Card.findById(req.params.cardId)
-    .orFail
+    .orFail(() => {
+      throw new NotFoundError('Карточка с указанным id не найдена.');
+    })
     .then((card) => {
       if (card.owner !== req.user._id) {
-        return res.status(FORBIDDEN_ERROR).send('Нет прав на удаление карточки');
+        throw new ForbiddenError('Недостаточно прав для удаления карточки.');
       }
       return Card.findByIdAndRemove(card._id)
         .then(() => {
           res.send(card);
         });
     })
-    .catch((err) => {
-      console.error(err);
-      if (err instanceof mongoose.Error.CastError) {
-        return res.status(BAD_REQUEST_ERROR).send({ message: 'Переданы некорректные данные для удаления карточки.' });
-      }
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        return res.status(NOT_FOUND_ERROR).send({ message: `Карточка с указанным _id:${req.params.cardId} не найдена.` });
-      }
-      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
-    });
+    .catch(next);
 };
 
-const toggleLikeStatus = (req, res, data, status) => {
+const toggleLike = (req, res, data, status, next) => {
   Card.findByIdAndUpdate(req.params.cardId, data, { new: true })
-    .orFail()
+    .orFail(() => {
+      throw new NotFoundError('Карточка с указанным id не найдена.');
+    })
     .then((card) => {
       res.status(status).send(card);
     })
-    .catch((err) => {
-      console.error(err);
-      if (err instanceof mongoose.Error.CastError) {
-        return res.status(BAD_REQUEST_ERROR).send({ message: 'Переданы некорректные данные.' });
-      }
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        return res.status(NOT_FOUND_ERROR).send({ message: `Передан несуществующий _id:${req.params.cardId} карточки.` });
-      }
-      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
-    });
+    .catch(next);
 };
 
-const likeCard = (req, res) => {
-  toggleLikeStatus(req, res, { $addToSet: { likes: req.user._id } }, CREATED_STATUS);
+const likeCard = (req, res, next) => {
+  toggleLike(req, res, { $addToSet: { likes: req.user._id } }, CREATED_STATUS, next);
 };
 
-const dislikeCard = (req, res) => {
-  toggleLikeStatus(req, res, { $pull: { likes: req.user._id } }, OK_STATUS);
+const dislikeCard = (req, res, next) => {
+  toggleLike(req, res, { $pull: { likes: req.user._id } }, OK_STATUS, next);
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -1,53 +1,39 @@
 const { JWT_SECRET } = process.env;
-const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const NotFoundError = require('../errors/NotFoundError');
 
-const DEFAULT_ERROR = 500;
-const NOT_FOUND_ERROR = 404;
-const UNAUTHORIZED_ERROR = 401;
-const BAD_REQUEST_ERROR = 400;
 const CREATED_STATUS = 201;
 
-const getUsers = (req, res) => {
+const getUsers = (req, res, next) => {
   User.find({})
     .then((users) => {
       res.send(users);
     })
-    .catch((err) => {
-      console.error(err);
-      res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
-    });
+    .catch(next);
 };
 
-const findUserById = (req, res, id) => {
+const findUserById = (req, res, id, next) => {
   User.findById(id)
-    .orFail()
+    .orFail(() => {
+      throw new NotFoundError('Пользователь с указанным id не найден.');
+    })
     .then((user) => {
       res.send(user);
     })
-    .catch((err) => {
-      console.error(err);
-      if (err instanceof mongoose.Error.CastError) {
-        return res.status(BAD_REQUEST_ERROR).send({ message: 'Переданы некорректные данные пользователя.' });
-      }
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        return res.status(NOT_FOUND_ERROR).send({ message: `Пользователь с указанным _id:${req.params.userId} не найден.` });
-      }
-      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
-    });
+    .catch(next);
 };
 
-const getUser = (req, res) => {
-  findUserById(req, res, req.params.userId);
+const getUser = (req, res, next) => {
+  findUserById(req, res, req.params.userId, next);
 };
 
-const getCurrentUser = (req, res) => {
-  findUserById(req, res, req.user._id);
+const getCurrentUser = (req, res, next) => {
+  findUserById(req, res, req.user._id, next);
 };
 
-const createUser = (req, res) => {
+const createUser = (req, res, next) => {
   const {
     name, about, avatar, email, password,
   } = req.body;
@@ -57,44 +43,31 @@ const createUser = (req, res) => {
       name, about, avatar, email, password: hash,
     }))
     .then((user) => res.status(CREATED_STATUS).send(user))
-    .catch((err) => {
-      console.error(err);
-      if (err instanceof mongoose.Error.ValidationError) {
-        return res.status(BAD_REQUEST_ERROR).send({ message: 'Переданы некорректные данные при создании пользователя.' });
-      }
-      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
-    });
+    .catch(next);
 };
 
-const updateUser = (req, res, data) => {
+const updateUser = (req, res, data, next) => {
   User.findByIdAndUpdate(req.user._id, data, { new: true, runValidators: true })
-    .orFail()
+    .orFail(() => {
+      throw new NotFoundError('Пользователь с указанным id не найден.');
+    })
     .then((user) => {
       res.send(user);
     })
-    .catch((err) => {
-      console.error(err);
-      if (err instanceof mongoose.Error.ValidationError) {
-        return res.status(BAD_REQUEST_ERROR).send({ message: 'Переданы некорректные данные' });
-      }
-      if (err instanceof mongoose.Error.DocumentNotFoundError) {
-        return res.status(NOT_FOUND_ERROR).send({ message: `Пользователь с указанным _id:${req.params.userId} не найден.` });
-      }
-      return res.status(DEFAULT_ERROR).send({ message: 'На сервере произошла ошибка' });
-    });
+    .catch(next);
 };
 
-const updateUserProfile = (req, res) => {
+const updateUserProfile = (req, res, next) => {
   const { name, about } = req.body;
-  updateUser(req, res, { name, about });
+  updateUser(req, res, { name, about }, next);
 };
 
-const updateUserAvatar = (req, res) => {
+const updateUserAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  updateUser(req, res, { avatar });
+  updateUser(req, res, { avatar }, next);
 };
 
-const login = (req, res) => {
+const login = (req, res, next) => {
   const { email, password } = req.body;
   User.findUserByCredentials(email, password)
     .then((user) => {
@@ -107,9 +80,7 @@ const login = (req, res) => {
         })
         .send(('Аутентификация успешно выполнена'));
     })
-    .catch((err) => {
-      res.status(UNAUTHORIZED_ERROR).send({ message: err.message });
-    });
+    .catch(next);
 };
 
 module.exports = {
