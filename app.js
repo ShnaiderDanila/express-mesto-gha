730170b6852ff78c9047295eaeaf81f9c6ecb02c
Shnaider Danila <shnd3r@yandex.ru>
2023-09-04 21:03:09 +0700

    feat: Include validation middleware functions in app.js and add an error handler for Joi-celebrate

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const { createUser, login } = require('./controllers/users');
 const routes = require('./routes/index');
 const auth = require('./middlewares/auth');
 const handleError = require('./errors/errorHandler');
+const { signUpValidation, signInValidation } = require('./middlewares/validation');
 
 const app = express();
 
@@ -18,12 +20,14 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signup', signUpValidation, createUser);
+app.post('/signin', signInValidation, login);
 
 app.use(auth);
 
 app.use(routes);
+
+app.use(errors());
 
 app.use(handleError);
 
