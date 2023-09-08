a4a9648fb50434a33d263e3f486b98f5754181e4
Shnaider Danila <shnd3r@yandex.ru>
2023-09-08 13:37:27 +0700

    fix: Add a validate property to the avatar field and remove the minlength property to the password field in ./models/user.js

--- models/user.js
+++ models/user.js
@@ -19,6 +19,12 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator(v) {
+        return validator.isURL(v);
+      },
+      message: 'Введен некорретный URL адрес',
+    },
   },
   email: {
     type: String,
@@ -34,7 +40,6 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 8,
     select: false,
   },
 });
