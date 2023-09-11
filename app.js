b6f50756bc7374150e4961814a6ba4746f5e6298
Shnaider Danila <shnd3r@yandex.ru>
2023-09-11 14:21:12 +0700

    feat: Connect the logger functions requestLogger, errorLogger to app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { createUser, login } = require('./controllers/users');
 const { signUpValidation, signInValidation } = require('./middlewares/validation');
 const auth = require('./middlewares/auth');
@@ -18,11 +19,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.post('/signup', signUpValidation, createUser);
 app.post('/signin', signInValidation, login);
 
 app.use(auth);
 app.use(routes);
+
+app.use(errorLogger);
+
 app.use(errors());
 app.use(handleErrors);
 
